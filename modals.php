a4c53c17d3cd51e023065bfbc7c2d6c65a2d1833
azad-2021 <suryavanshianantsingh@gmai.com>
2022-04-27 17:14:59 +0530

    files added

---
+++ modals.php
@@ -0,0 +1,278 @@
+<div class="modal fade" id="StaffF" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Find Staff</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Enter Staff Name</label>
+            <input type="text" class="form-control" id="FStaffName">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </form>
+        <button type="button" class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="StudentF" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Find Student</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Enter Student Name</label>
+            <input type="text" class="form-control" id="FStudentName">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </form>
+        <button type="button" class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="ReceiptF" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Find Fees Receipt</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Enter Receipt Number</label>
+            <input type="text" class="form-control" id="FReceiptNo">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </form>
+        <button type="button" class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="Registration" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Student Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center">
+          <div class="row">
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Student Name</label>
+              <input type="text" class="form-control" id="FReceiptNo">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Class</label>
+              <select class="form-control">
+                <option value="">Select</option>
+              </select>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Father's Name</label>
+              <input type="text" class="form-control" id="FReceiptNo">
+            </div>            
+
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Mother's Name</label>
+              <input type="text" class="form-control" id="FReceiptNo">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Gender:</label>
+              <br>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="Gender" id="Gender" value="Male">
+                <label class="form-check-label" for="flexRadioDefault1">
+                 Male
+               </label>
+             </div>
+             <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="Gender" id="Gender" value="Female">
+              <label class="form-check-label" for="flexRadioDefault2">
+                Female
+              </label>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <label for="recipient-name" class="col-form-label">AADHAR Card Number</label>
+            <input type="text" class="form-control" id="FReceiptNo">
+          </div>
+          <div class="col-lg-3">
+            <label for="recipient-name" class="col-form-label">Mobile Number</label>
+            <input type="text" class="form-control" id="FReceiptNo">
+          </div>
+          <div class="col-lg-3">
+            <label for="recipient-name" class="col-form-label">Address</label>
+            <input type="text" class="form-control" id="FReceiptNo">
+          </div>
+          <center>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Upload Image</label>
+              <input type="file" class="form-control" id="FReceiptNo">
+              <br>
+            </div>
+          </center>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </form>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="modal fade" id="AddStaff" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Staff Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center">
+          <div class="row">
+            <div class="col-lg-4">
+              <label for="recipient-name" class="col-form-label">Name</label>
+              <input type="text" class="form-control" id="FReceiptNo">
+            </div>
+            <div class="col-lg-4">
+              <label for="recipient-name" class="col-form-label">AADHAR Card Number</label>
+              <input type="text" class="form-control" id="FReceiptNo">
+            </div>
+            <div class="col-lg-4">
+              <label for="recipient-name" class="col-form-label">Gender:</label>
+              <br>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="Gender" id="Gender" value="Male">
+                <label class="form-check-label" for="flexRadioDefault1">
+                 Male
+               </label>
+             </div>
+             <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="Gender" id="Gender" value="Female">
+              <label class="form-check-label" for="flexRadioDefault2">
+                Female
+              </label>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <label for="recipient-name" class="col-form-label">Mobile Number</label>
+            <input type="text" class="form-control" id="FReceiptNo">
+          </div>
+          <div class="col-lg-4">
+            <label for="recipient-name" class="col-form-label">Address</label>
+            <input type="text" class="form-control" id="FReceiptNo">
+          </div>
+          <div class="col-lg-4">
+            <label for="recipient-name" class="col-form-label">Upload Resume</label>
+            <input type="file" class="form-control" id="FReceiptNo">
+            
+          </div>  
+          <center>
+            <div class="col-lg-6">
+              <label for="recipient-name" class="col-form-label">Educational Details</label>
+              <textarea class="form-control"></textarea>
+              <br>
+            </div>
+          </center>    
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </form>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="modal fade" id="AddSubject" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Subject Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center">
+          <div class="row">
+            <div class="col-lg-6">
+              <label for="recipient-name" class="col-form-label">Subject Name</label>
+              <input type="text" class="form-control" id="FReceiptNo">
+            </div>
+            <div class="col-lg-6">
+              <label for="recipient-name" class="col-form-label">Class</label>
+              <select class="form-control">
+                <option value="">Select</option>
+              </select>
+            </div>   
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </form>
+          <button type="button" class="btn btn-primary">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="AddSalary" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Release Salary</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center">
+          <div class="row">
+
+            <div class="col-lg-4">
+              <label for="recipient-name" class="col-form-label">Select Staff</label>
+              <select class="form-control">
+                <option value="">Select</option>
+              </select>
+            </div> 
+            <div class="col-lg-4">
+              <label for="recipient-name" class="col-form-label">Enter Amount</label>
+              <input type="number" class="form-control" id="FReceiptNo" min="0">
+            </div>  
+            <div class="col-lg-4">
+              <label for="recipient-name" class="col-form-label">Select Month</label>
+              <input type="month" class="form-control" id="FReceiptNo" min="0">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </form>
+          <button type="button" class="btn btn-primary">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ sidebar.php
@@ -0,0 +1,104 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link " href="index.php">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li><!-- End Dashboard Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-search"></i><span>Find</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="" data-bs-toggle="modal" data-bs-target="#StaffF">
+            <i class="bi bi-circle"></i><span>Staff</span>
+          </a>
+        </li>
+        <li>
+          <a href="" data-bs-toggle="modal" data-bs-target="#StudentF">
+            <i class="bi bi-circle"></i><span>Student</span>
+          </a>
+        </li>
+        <li>
+          <a href="" data-bs-toggle="modal" data-bs-target="#ReceiptF">
+            <i class="bi bi-circle"></i><span>Receipt No</span>
+          </a>
+        </li>
+      </ul>
+    </li><!-- End Components Nav -->
+  </li><!-- End Icons Nav -->
+
+  <li class="nav-heading">Management</li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="" data-bs-toggle="modal" data-bs-target="#Registration">
+      <i class="bi bi-person"></i>
+      <span>Student Registration</span>
+    </a>
+  </li><!-- End Register Page Nav -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="" href="" data-bs-toggle="modal" data-bs-target="#AddStaff">
+      <i class="bi bi-person"></i>
+      <span>Add Staff</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href=""  data-bs-toggle="modal" data-bs-target="#AddSubject">
+      <i class="bi bi-card-list"></i>
+      <span>Add Subject</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-faq.html">
+      <i class="bi bi-person"></i>
+      <span>Staff List</span>
+    </a>
+  </li><!-- End F.A.Q Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-contact.html">
+      <i class="bi bi-person"></i>
+      <span>Student List</span>
+    </a>
+  </li><!-- End Contact Page Nav -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-faq.html">
+      <i class="bi bi-card-list"></i>
+      <span>Subject List</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-login.html">
+      <i class="bx bx-rupee"></i>
+      <span>Pending Fees</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="" href="" data-bs-toggle="modal" data-bs-target="#AddSalary">
+      <i class="bi bi-person"></i>
+      <span>Update Salary</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-error-404.html">
+      <i class="bx bx-rupee"></i>
+      <span>Salary Status</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-blank.html">
+      <i class="bi bi-file-earmark"></i>
+      <span>Print ID Card</span>
+    </a>
+  </li>
+</ul>
+
+</aside>
+  <!-- End Sidebar-->
