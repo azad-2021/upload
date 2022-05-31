883634dd900159099eff412c2e88992b1bb7fe41
azad-2021 <suryavanshianantsingh@gmai.com>
2022-05-31 12:49:06 +0530

    dark erp added

---
+++ admin-temp/modals.php
@@ -0,0 +1,330 @@
+<!-- Modal -->
+<div class="modal fade" id="Registration" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Student Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center" method="POST" action="" enctype="multipart/form-data" style="color:white;">
+          <div class="row">
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Student Name</label>
+              <input style="color:white" style="color:white" type="text" class="form-control" name="StudentName">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Class</label>
+              <select style="color:white" class="form-control" name="StudentClass" required="">
+                <option value="">Select</option>
+                <?php 
+                $query ="SELECT * FROM class";
+                $result = mysqli_query($con, $query);
+                if (mysqli_num_rows($result)>0){
+                  while($row=mysqli_fetch_assoc($result)){
+                    echo '<option value="'.$row['ClassID'].'">'.$row['Class'].'</option>';
+                  }}?>
+              </select>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Father's Name</label>
+              <input style="color:white" type="text" class="form-control" name="Father" required="">
+            </div>            
+
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Mother's Name</label>
+              <input style="color:white" type="text" class="form-control" name="Mother" required="">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Gender:</label>
+              <br>
+              <div class="form-check form-check-inline">
+                <input style="color:white" class="form-check-input" type="radio" name="Gender" id="Gender" value="Male" required="">
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Male
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input style="color:white" class="form-check-input" type="radio" name="Gender" id="Gender" value="Female">
+                <label class="form-check-label" for="flexRadioDefault2">
+                  Female
+                </label>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">AADHAR Card Number</label>
+              <input style="color:white" type="number" class="form-control" name="Aadhar" min="0" required="">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Mobile Number</label>
+              <input style="color:white" type="number" class="form-control" name="Mobile" min="0" required="">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Address</label>
+              <input style="color:white" style="color:white" type="text" class="form-control" name="Address">
+            </div>
+            <center>
+              <div class="col-lg-3">
+                <label for="recipient-name" class="col-form-label">Upload Image</label>
+                <input style="color:white" style="color:white" type="file" class="form-control" name = "image" / required="">
+                <br>
+              </div>
+            </center>
+
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+            <button type="submit" class="btn btn-primary" name="SaveStudent">Save</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="AddStaff" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Staff Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center" method="POST" action="" enctype="multipart/form-data">
+          <div class="row">
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Name</label>
+              <input style="color:white" type="text" class="form-control" name="StaffName" required>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">AADHAR Card Number</label>
+              <input style="color:white" type="number" class="form-control" name="StaffAadhar" required min="0" maxlength="12">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Salary Amount</label>
+              <input style="color:white" type="number" class="form-control" name="SalaryAmount" required min="0" maxlength="16">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Gender:</label>
+              <br>
+              <div class="form-check form-check-inline">
+                <input style="color:white" class="form-check-input" type="radio" name="Gender" value="Male" required>
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Male
+                </label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input style="color:white" class="form-check-input" type="radio" name="Gender" value="Female">
+                <label class="form-check-label" for="flexRadioDefault2">
+                  Female
+                </label>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Mobile Number</label>
+              <input style="color:white" type="text" class="form-control" name="StaffNumber" onkeydown="limit(this);" onkeyup="limit(this);" length=10 required>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Email</label>
+              <input style="color:white" type="email" class="form-control" name="StaffEmail" required>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Address</label>
+              <input style="color:white" type="text" class="form-control" name="StaffAddress" required>
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Upload Resume</label>
+              <input style="color:white" type="file" class="form-control" name = "Resume" />
+            </div>  
+            <center>
+              <div class="col-lg-6">
+                <label for="recipient-name" class="col-form-label">Educational Details</label>
+                <textarea class="form-control" name="StaffEducation" required></textarea>
+                <br>
+              </div>
+            </center>    
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary" name="SaveStaff">Save</button>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="AddSubject" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Subject Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center" id="Fsubject">
+          <div class="row">
+            <div class="col-lg-6">
+              <label for="recipient-name" class="col-form-label">Subject Name</label>
+              <input style="color:white" type="text" class="form-control" id="subject">
+            </div>
+            <div class="col-lg-6">
+              <label for="recipient-name" class="col-form-label">Class</label>
+              <select style="color:white" class="form-control" id="SubClass">
+                <option value="">Select</option>
+                <?php 
+                $query ="SELECT * FROM class";
+                $result = mysqli_query($con, $query);
+                if (mysqli_num_rows($result)>0){
+                  while($row=mysqli_fetch_assoc($result)){
+                    echo '<option value="'.$row['ClassID'].'">'.$row['Class'].'</option>';
+                  }}?>
+              </select>
+            </div>   
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </form>
+          <button type="button" class="btn btn-primary SaveSubject">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="AddSalary" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Release Salary</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center" id="SalaryForm">
+          <div class="row">
+
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Select Staff</label>
+              <select style="color:white" class="form-control" id="StaffIDS">
+                <option value="">Select</option>
+                <?php 
+                $query ="SELECT * FROM staff WHERE Inservice=1";
+                $result = mysqli_query($con, $query);
+                if (mysqli_num_rows($result)>0){
+                  while($row=mysqli_fetch_assoc($result)){
+                    echo '<option value="'.$row['StaffID'].'">'.$row['StaffName'].'</option>';
+                  }}?>
+              </select>
+            </div> 
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Salary Amount</label>
+              <input style="color:white" type="text" class="form-control" id="SalaryAmount" min="0" disabled="">
+            </div>
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Enter Amount</label>
+              <input style="color:white" type="number" class="form-control" id="Salary" min="0">
+            </div>  
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Select Month</label>
+              <input style="color:white" type="month" class="form-control" id="SalaryMonth">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </form>
+          <button type="button" class="btn btn-primary SaveSalary">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="SubjectList" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">All Subjects</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <center>
+          <input style="color:white" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for subject.." class="form-control">
+        </center>
+        <br>
+        <table class="table table-hover table-bordered border-primary" id="myTable">
+          <thead>
+            <th>Sr No.</th>
+            <th>Subject Name</th>
+            <th>Class</th>
+            <th>Action</th>
+            <tbody id="sublist">
+
+            </tbody>
+          </thead>
+        </table>
+      </div>   
+      
+      <div class="modal-footer">
+        <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="AddFees" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content rounded-corner">
+      <div class="modal-header rounded-corner">
+        <h5 class="modal-title">Enter Fees Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form align="center" id="FeesForm">
+          <div class="row">
+            <div class="col-lg-3">
+              <label for="recipient-name" class="col-form-label">Class</label>
+              <select style="color:white" class="form-control" id="FeesClass">
+                <option value="">Select</option>
+                <?php 
+                  $query ="SELECT * FROM class";
+                  $result = mysqli_query($con, $query);
+                  if (mysqli_num_rows($result)>0){
+                    while($row=mysqli_fetch_assoc($result)){
+                      echo '<option value="'.$row['ClassID'].'">'.$row['Class'].'</option>';
+                    }}?>
+                </select>
+              </div>
+              <div class="col-lg-3">
+                <label for="recipient-name" class="col-form-label">Select Student</label>
+                <select style="color:white" class="form-control" id="FeesStudent">  
+                  <option>Select</option>
+                </select>
+              </div>
+              <div class="col-lg-3">
+                <label for="recipient-name" class="col-form-label">Select Month</label>
+                <input style="color:white" type="month" name="" id="FeesMonth">
+              </div>
+              <div class="col-lg-3">
+                <label for="recipient-name" class="col-form-label">Total Amount</label>
+                <input style="color:white" type="text" class="form-control" id="Fees" min="0" disabled>
+              </div>
+              <center>
+                <div class="col-lg-3">
+                  <label for="recipient-name" class="col-form-label">Paid Amount</label>
+                  <input style="color:white" type="number" class="form-control" id="FeesAmount" min="0">
+                </div>
+              </div>
+            </center>
+            <div class="modal-footer">
+              <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </form>
+            <button type="button" class="btn btn-primary SaveFees">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ admin-temp/nav.php
@@ -0,0 +1,195 @@
+    <!-- partial:partials/_sidebar.html -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+        <a class="sidebar-brand brand-logo" href="index.html"><img src="assets/images/logo.webp" alt="logo" /></a>
+        <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo.webp" alt="logo" /></a>
+      </div>
+      <ul class="nav">
+        <li class="nav-item profile">
+          <div class="profile-desc">
+            <div class="profile-pic">
+              <div class="count-indicator">
+                <img class="img-xs rounded-circle " src="assets/images/faces/face15.jpg" alt="">
+                <span class="count bg-success"></span>
+              </div>
+              <div class="profile-name">
+                <h5 class="mb-0 font-weight-normal">ABC</h5>
+                <span>Admin</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item nav-category">
+          <span class="nav-link">Navigation</span>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="index.html">
+            <span class="menu-icon">
+              <i class="mdi mdi-speedometer"></i>
+            </span>
+            <span class="menu-title">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <span class="menu-icon">
+              <i class="mdi mdi-laptop"></i>
+            </span>
+            <span class="menu-title">Add</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="ui-basic">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#Registration">Student</a></li>
+              <li class="nav-item"> <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#AddStaff">Staff</a></li>
+              <li class="nav-item"> <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#AddSubject">Subject</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <span class="menu-icon">
+              <i class="mdi mdi-security"></i>
+            </span>
+            <span class="menu-title">Update</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="auth">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html">Class Fees</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html">Class Subject</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html">Staff Salary</a></li>
+            </ul>
+          </div>
+        </li>
+
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#AddFees">
+            <span class="menu-icon">
+              <i class="mdi mdi-chart-bar"></i>
+            </span>
+            <span class="menu-title">Student Fees</span>
+          </a>
+        </li>
+
+
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#AddSalary">
+            <span class="menu-icon">
+              <i class="mdi mdi-chart-bar"></i>
+            </span>
+            <span class="menu-title">Staff Salary</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="pages/forms/basic_elements.html">
+            <span class="menu-icon">
+              <i class="mdi mdi-playlist-play"></i>
+            </span>
+            <span class="menu-title">Students</span>
+          </a>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="stafflist.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-table-large"></i>
+            </span>
+            <span class="menu-title">Staff</span>
+          </a>
+        </li>
+
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#SubjectList">
+            <span class="menu-icon">
+              <i class="mdi mdi-chart-bar"></i>
+            </span>
+            <span class="menu-title">Subjects</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="pages/charts/chartjs.html">
+            <span class="menu-icon">
+              <i class="mdi mdi-chart-bar"></i>
+            </span>
+            <span class="menu-title">Pending Fess</span>
+          </a>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="pages/icons/mdi.html">
+            <span class="menu-icon">
+              <i class="mdi mdi-contacts"></i>
+            </span>
+            <span class="menu-title">Pending Salary</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_navbar.html -->
+      <nav class="navbar p-0 fixed-top d-flex flex-row">
+        <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+        </div>
+        <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
+          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="mdi mdi-menu"></span>
+          </button>
+          <ul class="navbar-nav w-100">
+            <li class="nav-item w-100">
+              <h4>Good Morning ABC</h4>
+            </li>
+          </ul>
+          <ul class="navbar-nav navbar-nav-right">
+
+            <li class="nav-item nav-settings d-none d-lg-block">
+              <a class="nav-link" href="#">
+                <i class="mdi mdi-view-grid"></i>
+              </a>
+            </li>
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link" id="profileDropdown" href="#" data-bs-toggle="dropdown">
+                <div class="navbar-profile">
+                  <img class="img-xs rounded-circle" src="assets/images/faces/face15.jpg" alt="">
+                  <p class="mb-0 d-none d-sm-block navbar-profile-name">ABC</p>
+                  <i class="mdi mdi-menu-down d-none d-sm-block"></i>
+                </div>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="profileDropdown">
+                <h6 class="p-3 mb-0">Profile</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-dark rounded-circle">
+                      <i class="mdi mdi-settings text-success"></i>
+                    </div>
+                  </div>
+                  <div class="preview-item-content">
+                    <p class="preview-subject mb-1">Settings</p>
+                  </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-dark rounded-circle">
+                      <i class="mdi mdi-logout text-danger"></i>
+                    </div>
+                  </div>
+                  <div class="preview-item-content">
+                    <p class="preview-subject mb-1">Log out</p>
+                  </div>
+                </a>
+              </div>
+            </li>
+          </ul>
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="mdi mdi-format-line-spacing"></span>
+          </button>
+        </div>
+      </nav>
